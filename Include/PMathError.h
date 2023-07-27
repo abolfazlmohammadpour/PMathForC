8e1b8bc9b5290824fc21ab0a71a27cc1d329fdd3
AbolfazlMohammadpour <abolfazlmohammadpourqaemshahr@gmail.com>
2023-07-27 14:15:23 +0330

    Add <Showing_PMathError> And <Exiting_WithError> To <Include/PMathError.h>

---
+++ Include/PMathError.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define No_Error (unsigned short int)1383
+#define Error_1384 (unsigned short int)1384
+
+unsigned short int PMathError;
+PMathError = No_Error;
+
+// This Function Showing Message Of PMathError
+void Showing_PMathError(unsigned short int PMathError)
+{
+    switch (PMathError)
+    {
+    case No_Error:
+    {
+        break;
+    }
+    case Error_1384:
+    {
+        printf("%c%s%c", '\a', "Error: From <Calculating_NumberOfDivisors> \"The Input Argument Does Not Have To Be Zero\".", '\n');
+        break;
+    }
+    }
+}
+
+// This Function Showing Message Of PMathError And Close The Program
+void Exiting_WithError(unsigned short int Error)
+{
+    PMathError = Error;
+
+    Showing_PMathError(PMathError);
+
+    exit((int)-1);
+}
