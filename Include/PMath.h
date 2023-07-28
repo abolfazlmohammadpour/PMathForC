4f4fb7b16e4366115c949f9decbb8d41b994648e
AbolfazlMohammadpour <abolfazlmohammadpourqaemshahr@gmail.com>
2023-07-28 12:52:41 +0330

    Add <Calculating_Divisors> To <Include/PMath.h>

--- Include/PMath.h
+++ Include/PMath.h
@@ -7,7 +7,7 @@ void Calculating_NumberOfDivisors(long long int Number, long long int *NumberOfD
 
     if (Number == (long long int)0)
     {
-        ExitingWithError(Error_1384);
+        Exiting_WithError(Error_1384);
         return;
     }
     else if (Number > (long long int)0)
@@ -28,6 +28,42 @@ void Calculating_NumberOfDivisors(long long int Number, long long int *NumberOfD
             if ((Number % Counter) == (long long int)0)
             {
                 *NumberOfDivisors += (long long int)1;
+            }
+        }
+        return;
+    }
+}
+
+// This Function Calculates Divisors Of Argument <Number> And Places Them In Argument <Divisors>
+void Calculating_Divisors(long long int Number, long long int *Divisors)
+{
+    long long int IndexOfDivisor = (long long int)0;
+
+    if (Number == (long long int)0)
+    {
+        Exiting_WithError(Error_1385);
+        return;
+    }
+    else if (Number > (long long int)0)
+    {
+        for (long long int Counter = (long long int)1; Counter <= Number; Counter++)
+        {
+            if ((Number % Counter) == (long long int)0)
+            {
+                *(Divisors + IndexOfDivisor) = Counter;
+                IndexOfDivisor++;
+            }
+        }
+        return;
+    }
+    else
+    {
+        for (long long int Counter = (long long int)1; Counter <= (-Number); Counter++)
+        {
+            if ((Number % Counter) == (long long int)0)
+            {
+                *(Divisors + IndexOfDivisor) = Counter;
+                IndexOfDivisor++;
             }
         }
         return;
